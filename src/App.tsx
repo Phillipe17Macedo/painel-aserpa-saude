a75fb413ab74bc6d6eba373978a1fc3715120a9c
Phillipe17Macedo <ph.stos.udia@gmail.com>
2024-07-10 10:30:31 -0300

    configurado-envio-notificações-all

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import NotificationForm from './Notification/NotificationForm';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Painel Aserpa Saúde</h1>
       </header>
+      <main>
+        <NotificationForm />
+      </main>
     </div>
   );
 }
